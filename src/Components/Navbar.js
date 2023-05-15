5de24a5e4ec9cbf4ac70cd7f9108b386e0bd9b52
Subhadip kundu <124190254+subhadip-kundu@users.noreply.github.com>
2023-05-15 22:24:52 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,39 +1,39 @@
 import React from "react"
-
-export default function Navbar(props){
-    return(
-        <nav className="navbar navbar-expand-lg bg-body-tertiary navbar-dark">
-    <div className="container-fluid bg-dark">
-        <a className="navbar-brand" href="#">{props.title}</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul className="dropdown-menu">
-                        <li><a className="dropdown-item" href="#">Action</a></li>
-                        <li><a className="dropdown-item" href="#">Another action</a></li>
-                        <li><hr className="dropdown-divider" /></li>
-                        <li><a className="dropdown-item" href="#">Something else here</a></li>
+import {Link } from 'react-router-dom';
+export default function Navbar(props) {
+    return (
+        <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode} `}>
+            <div className={`container-fluid bg-${props.mode}`}>
+                <Link className="navbar-brand" to="/">{props.title}</Link>
+                <button className="navbar-toggler p-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse " id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item mx-2 ">
+                            <Link className="nav-link active fs-5" aria-current="page" to="/">Home</Link>
+                        </li>
+                        <li className="nav-item mx-2 ">
+                            <Link className="nav-link active fs-5" aria-current="page" to="/about">About us</Link>
+                        </li>
+                        <li className="nav-item mx-2 ">
+                            <Link className="nav-link active fs-5" aria-current="page" to="/">Services</Link>
+                        </li>
                     </ul>
-                </li>
-            </ul>
-            <form className="d-flex" role="search">
-                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                <button className="btn btn-outline-primary" type="submit">Search</button>
-            </form>
-        </div>
-    </div>
-</nav>
+
+                    
+
+                    <form className="d-flex mx-5" role="search">
+                        <input className="form-control me-2 " type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-primary" type="submit">Search</button>
+                    </form>
+                    <div className="form-check form-switch ">
+                        <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                        <label className={`form-check-label fs-6 align-middle text-${props.mode==='light'?'dark':'light'}`} htmlFor="flexSwitchCheckDefault">Change Mode</label>
+                    </div>
+                </div>
+            </div>
+        </nav>
     );
 }
+
