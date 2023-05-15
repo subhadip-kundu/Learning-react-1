82822e23ac5a96e101d8b5bd9b4df4fa12834dd0
Subhadip kundu <124190254+subhadip-kundu@users.noreply.github.com>
2023-05-15 22:25:09 +0530

    Update TextForm.js

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -5,48 +5,59 @@ export default function TextForm(props) {
     const handleUpClick = () => {
         let newText = text.toUpperCase();
         setText(newText);
+        props.showAlert("Converted to uppercase!", "success")
     }
     const handleUpClickTolowercase = () => {
         let newText = text.toLowerCase();
         setText(newText);
+        props.showAlert("Converted to Lowercase!", "success")
     }
-    // const handleUpClickToCapitalize = () => {
-    //     let newText = text;
-    //     setText(newText);
-    // }
+    const handleUpClickToRemoveSpace = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(' '));
+        props.showAlert("Extra space removed!", "success");
+    }
     const handleUpClickToclear = () => {
         setText('');
+        props.showAlert("Textarea Cleared!", "success")
     }
 
     const handleOnChange = (event) => {
-        console.log("Done");
         setText(event.target.value);
     }
 
-   
+    const handleUpClickTocopy = () => {
+        navigator.clipboard.writeText(text);
+        props.showAlert("Textarea Copied!", "success");
+    }
+
 
     const [text, setText] = useState('');
     return (
         <>
-
-            <div>
-                <div className="mb-3">
-                    <label htmlFor="myBox" className="form-label">{props.lebel}</label>
-                    <textarea className="form-control" id="exampleFormControlTextarea1" value={text} onChange={handleOnChange} rows="4"></textarea>
+            <div className="container" style={{ color: props.mode === 'dark' ? '#fff' : '#051b3c' }}>
+                <div>
+                    <div className="mb-3">
+                        <label htmlFor="myBox" className="form-label">{props.heading}</label>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark' ? '#415d89' : '#fff', color: props.mode === 'dark' ? '#fff' : '#083b6e' }} rows="7"></textarea>
+                    </div>
+                    <button disabled={text.length === 0} className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+                    <button disabled={text.length === 0} className="btn btn-primary mx-3" onClick={handleUpClickTolowercase}>Convert to Lowercse</button>
+                    <button disabled={text.length === 0} className="btn btn-primary mx-3" onClick={handleUpClickToRemoveSpace}>Remove extra space</button>
+                    <button disabled={text.length === 0} className="btn btn-primary" id="myBox" onClick={handleUpClickTocopy}>Copy</button>
+                    <button disabled={text.length === 0} className="btn btn-primary mx-3" onClick={handleUpClickToclear}>Clear</button>
                 </div>
-                <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
-                <button className="btn btn-primary mx-3" onClick={handleUpClickTolowercase}>Convert to Lowercse</button>
-                {/* <button className="btn btn-primary mx-3" onClick={handleUpClickToCapitalize}>Convert to Capitalize</button> */}
-                <button className="btn btn-primary " onClick={handleUpClickToclear}>Clear</button>
-            </div>
-            <div className="container my-4">
-                <h1>The text summary</h1>
-                <p>{text.split(" ").length-1} words and {text.length} characters</p>
-                <p>{text.split(" ").length *0.01} Minutes to read</p>
-            </div>
+                <div className="container my-4">
+                    <h1>The text summary</h1>
+                    <p>{text.split(/\s+/).filter((ele) => { return ele.length !== 0 }).length} words and {text.length} characters</p>
+                    <p>{(text.split(" ").length - 1) * 0.01} Minutes to read</p>
+                </div></div>
         </>
     )
 }
+
+
+
 
 
 
