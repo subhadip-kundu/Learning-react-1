92fee5b09d6859e5bd7eb3032222e2244c915982
Subhadip kundu <124190254+subhadip-kundu@users.noreply.github.com>
2023-05-15 22:23:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
+import { useState } from 'react';
 import './App.css';
 import About from './Components/About';
-import Navbar from './Components/Navbar'
-// import TextForm from './Components/TextForm';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
+import Alert from './Components/Alert';
 
-let name = "SUBHADIP";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
 
 function App() {
-    return (
-        <>
-            <Navbar title="React"/>
-            <div className="coder">
-                <h1>Hello, {name}</h1>
-            </div>
+  let [mode, setMode] = useState('light');
+  const [alert, setalert] = useState(null)
 
-            {/* <div className="container my-3" >
-            <TextForm lebel="Enter any text here to get it on Uppercase/Lowercase : "/>
-            </div> */}
+  const showAlert = (message, type) => {
+    setalert({
+      msg: message,
+      type: type
+    })
+  }
 
-            <About/>
-        </>
-    );
+  setTimeout(() => {
+    setalert(null);
+  }, 1500)
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#083b6e';
+      showAlert("Dark mode has been enabled", "success")
+      document.title = 'Text transformer - Dark mode'
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = '#fff';
+      showAlert("Light mode has been enabled", "success")
+      document.title = 'Text transformer - Light mode'
+    }
+  }
+
+  return (
+    <Router>
+      <Navbar title="React" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container">
+        <Routes>
+          <Route exact path="/about" element={<About mode={mode} />} />
+          <Route exact path="/" element={<TextForm heading="Enter text to analyze" mode={mode} showAlert={showAlert} />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
